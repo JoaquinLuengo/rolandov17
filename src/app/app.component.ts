22283df6ba2b16379598019c0bb0103434a761fb
administrador <joaquinluengo.05@gmail.com>
2024-02-08 19:28:27 -0300

    navbar y login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,23 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
+import { NavbarComponent } from './shaders/navbar/navbar.component';
+import { SidebarComponent } from './shaders/sidebar/sidebar.component';
+import { OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet , LoginComponent],
+  imports: [CommonModule, RouterOutlet , LoginComponent,NavbarComponent,SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'rolandov17';
 
-  hola:string = "hola";
+  ngOnInit(): void {
+    initFlowbite();
+  }
+
 }
